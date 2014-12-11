49d7ce2353d688df5f73e14018e8f78f61256100
ythalorossy <ythalorossy@gmail.com>
2014-12-11 11:14:14 -0300

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myYoProjectApp
+ * @description
+ * # myYoProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myYoProjectApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
